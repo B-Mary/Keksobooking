c65e33521788eb0008c1d0aaa11d242108c38a87
mary <mari-bahlina@ukr.net>
2021-06-05 10:12:44 +0300

    Added main.js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let array=[{
+  author: {
+    avatar: 'img/avatars/user{{xx}}.png'
+  },
+  offer: {
+    title: 'Живите комфортно',
+    address: '',
+    price: 20 ,
+    type: 'palce, flat, house, bungalow ',
+    rooms: 5 ,
+    quests: 4 ,
+    checkin: '12:00, 13:00, 14:00',
+    checkout: '12:00, 13:00, 14:00',
+    features:['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+    description: '',
+    photos:['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'] 
+  },
+  location:{
+    x: 35.65000,
+    y:139.70000 ,
+  }
+}]
