68f45081d13751dddd8add9df92c779470ca25c9
mary <mari-bahlina@ukr.net>
2021-06-24 20:04:13 +0300

    Task4 finall version

--- js/form.js
+++ js/form.js
@@ -1,51 +1,81 @@
 'use strict';
   
-  let form = document.querySelector('.ad-form__element');
-  let typeHome = document.getElementById("type");
-  let formPrice = document.getElementById("price");
+let form = document.querySelector('.ad-form__element');
+let typeHome = document.getElementById("type");
+let formPrice = document.getElementById("price");
+
+
+
+typeHome.addEventListener('change', homePrice )
 
 export function homePrice(evt){
-  
-  const typeHome =evt.target.value
- 
+
+  const typeHome =evt.target.value;
+  formPrice.value='';
+  let count = null
   switch (typeHome){
-    case "bungalow":
-      formPrice.setAttribute("placeholder", "0");
-      break;
-    case "flat":
-      formPrice.setAttribute("placeholder", "1000");
-      break;
-    case "house":
-      formPrice.setAttribute("placeholder", "5000");
-      break;
-    case "palace":
-      formPrice.setAttribute("placeholder", "10000");
-      break;
+   case "bungalow":
+    count = "0";      
+    break;
+   case "flat":
+    count = "1000";   
+    break;
+   case "house":
+    count ="5000";
+    break;
+   case "palace":
+    count= "10000";       
+    break;
   }
-  
+  formPrice.setAttribute("placeholder", count)
+  formPrice.min = count;
 }
 
 
-
-
-
-let formTime = document.querySelector('.ad-form__element--time');
+let formEl = document.querySelector('.ad-form__element--time');
 
 let timeIn = document.getElementById("timein")
 let timeOut = document.getElementById("timeout")
 
-timeIn.addEventListener('change',inTime )
-timeOut.addEventListener('change', outTime)
+timeIn.addEventListener('change', inTime )
+timeOut.addEventListener('change', outTime )
 
 
 export function inTime(evt){
-  const timeIn  =evt.target.value
-  document.getElementById("timeout").value = document.getElementById("timein").value
- }
-
-export function outTime(evt){
-  const timeOut  =evt.target.value
-  document.getElementById("timein").value = document.getElementById("timeout").value
+  const timeIn  =evt.target.value;
+  let count = null;
+  switch (timeIn){
+    case "12:00":
+     count = "12:00"
+      break;
+    case "13:00":
+    count = "13:00"
+      break;
+    case "14:00":
+     count = "14:00"
+      break;
   }
+ timeOut.value = count;
+}
+
+
+export function outTime(e){
+  const timeOut  =e.target.value; 
+  let cc =null
+  switch (timeOut){
+    case "12:00":
+      cc= "12:00"
+      break;
+    case "13:00":
+      cc = "13:00"
+      break;
+    case "14:00":
+      cc = "14:00"
+      break;
+  }
+ timeIn.value = cc;
+} 
+
+
  
 
--- js/main.js
+++ js/main.js
@@ -6,4 +6,3 @@ import {cloneCard} from "./card.js"
 import {homePrice, inTime, outTime} from "./form.js"
 
 
-typeHome.addEventListener('change', homePrice )
